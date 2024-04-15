7875e3377958b6af8ffc228a0b25e8c902f33803
Mo Johnson <mozeyj@gmail.com>
2024-04-15 13:31:33 -0600

    started on API javascript on movie search

--- js/news.js
+++ js/news.js
@@ -1,3 +1,17 @@
-function randomizer {
-    
+fetch("https://qva0myalaa.execute-api.us-east-1.amazonaws.com/moviesData")
+    .then((response) => response.json())
+    .then((data) => handleData(movieData));
+
+function handleData(movieData) {
+    let randomMovieData = randomizer(movieData);
+}
+
+function MovieSearch(movieData) {
+
+}
+
+function randomizer(radomMovieData) {
+    let decade = Math.floor(Math.random() * 10) + 1980;
+    console.log(decade);
+    let decadeData = movieData[decade];
 }
